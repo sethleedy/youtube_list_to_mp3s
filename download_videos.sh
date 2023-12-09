24fc8926703d350cd7662496ca00d5741774e3ec
Webmaster Seth <code@sethleedy.name>
2023-12-09 22:49:40 +0000

    FirstCommitToGitea

---
+++ download_videos.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+# Download videos from youtube channel list (channels.txt)
+# Inpsired by https://news.ycombinator.com/item?id=21509523
+
+# Using https://github.com/yt-dlp/yt-dlp for downloads
+
+
+# Gets the location of the script
+pdir="$( cd "$(dirname "$0")" ; pwd -P )"
+ffmpeg_location="$(which ffmpeg)"
+# where to drop our files downloaded/converted files
+channels_path="/sdcard/Podcasts/channels/"
+
+# make sure we have a channel list
+if [[ ! -a "$pdir/channels.txt" ]]; then
+	echo "channels.txt not found. Exiting."
+else
+	channels_file="$pdir/channels.txt"
+fi
+
+# Prep the channel folders
+#if [ ! -d "$pdir/channels" ]; then
+if [ ! -d "$channels_path" ]; then
+	mkdir "$channels_path"
+fi
+
+while read line; do
+
+	# If the line is not empty...
+	if [ ! -z "$line" ]; then
+
+	# Channel name and channel url are stored in channels.txt
+	read -r channel url <<< $(echo $line)
+
+	chan_path="$channels_path/$channel"
+	if [ ! -d "$chan_path" ]; then
+		mkdir "$chan_path"
+		touch "$chan_path/dl_archive.txt"
+	fi
+	cd $chan_path
+
+	video_filters="--dateafter now-7days --playlist-end 5"
+	output_options="--download-archive dl_archive.txt -o %(upload_date)s%(fulltitle)s.%(ext)s"
+	misc_options="--no-call-home --no-progress -N 2"
+
+	# 1440p or less, best audio we can find.
+	# Sometimes it complains about not being able to remux (webm+mp4). It falls back to mkv...
+#	video_options="-f bestvideo[height<=1440]+bestaudio"
+	video_options="-x --audio-format mp3"
+
+	options="$video_filters $video_options $output_options $misc_options --ffmpeg-location $ffmpeg_location"
+	# I wanted to keep youtube-dl out of here, but this seems like the best way to make this portable.
+	$pdir/yt-dlp $options -- "$url"
+
+	fi
+
+done <$channels_file
+
+cd "$pdir"
+
+
+# Deal with the files.
+# Right now, removing any files beyond an age.
+# -r is the age at which to start removing the files.
+# -t is the target directory.
+# These are preset in the script.
+$pdir/sync_files.sh -r 3
---
+++ sync_files.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+## Arguments
+# Remove audio files older than 8 days|or remove all.
+# By default it uses the preset variable in code of 8 days: $0 -r 8
+# $0 -r all
+
+# Default preset for the target directory to remove files. You can preset this in the variable below.
+# $0 -t "/sdcard/Podcasts/channels/"
+##
+
+## Default presets
+# Preset target directory if not passed as argument.
+preset_target_dir="/sdcard/Podcasts/channels/"
+# How old in Days should it be to remove?
+file_age=3
+remove_all_files=false
+##
+
+# Gets the location of the script
+pdir_syncs="$( cd "$(dirname "$0")" ; pwd -P )"
+
+# What file type is it we looking to remove?
+# I am using .mp3 at the moment.
+file_type="*.mp3"
+
+
+while getopts 'r:t:h' opt; do
+  case "$opt" in
+    r)
+	# If OPTARG is a number, then remove all older days of that number
+	if [[ $OPTARG == ?(-)+([[:digit:]]) ]]; then
+		file_Age="$OPTARG"
+		echo "Removing audio files older than $file_age days."
+	elif [ $OPTARG = "all" ]; then
+	# Else if OPTARG is 'all' then remove all audio files.
+		echo "Removing ALL audio files!"
+		remove_all_files=true
+	fi
+      ;;
+
+    t)
+	echo "Target directory is: $OPTARG"
+	preset_target_dir="$OPTARG"
+
+	# Sanity Check. The path should not be root and it should end with a slash.
+	# if ...
+      ;;
+
+    ?|h)
+      	echo ""
+	echo "Usage: $(basename $0) [-r 8|all] [-t \"/path/\"]"
+	echo ""
+	echo "[Removing older files]"
+	echo "Remove audio files older than set days|or remove all."
+	echo "Default preset variable in the code is set to $file_age days."
+	echo ""
+	echo "Set with '$0 -r $file_age'"
+	echo "To remove all audio files."
+	echo "$0 -r all"
+	echo ""
+	echo "[Change the target directory for storing files]"
+	echo "Default preset variable for the target directory to remove files is $preset_target_dir"
+	echo "It can be changed with."
+	echo "$0 -t $preset_target_dir"
+      exit 1
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+
+# Remove all or by age.
+if $remove_all_files; then
+	# Sanity check. Not / (root) or something else.
+	# if ...
+	echo "Removing files..."
+	#echo $preset_target_dir*/*.mp3
+	rm -rf $preset_target_dir*/*.mp3
+else
+
+	# Remove all *.mp3 files that are beyond an age, so all old ones are removed and do not fill our storage up.
+	#find "$preset_target_dir" -name "$file_type" -type f -mtime +$file_age
+	find "$preset_target_dir" -name "$file_type" -type f -mtime +$file_age -delete
+fi
