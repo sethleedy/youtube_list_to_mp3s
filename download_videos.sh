50c3a4b533d526cc095dbb0536b1c12965bc6670
Webmaster Seth <code@sethleedy.name>
2023-12-11 06:06:50 +0000

    Changed variables to ${} pattern

--- download_videos.sh
+++ download_videos.sh
@@ -17,34 +17,34 @@ channels_path="/LeedyData/leedymedia/audio_from_youtube"
 content_age=3
 
 # make sure we have a channel list
-if [[ ! -a "$pdir/channels.txt" ]]; then
+if [[ ! -a "${pdir}/channels.txt" ]]; then
 	echo "channels.txt not found. Exiting."
 else
-	channels_file="$pdir/channels.txt"
+	channels_file="${pdir}/channels.txt"
 fi
 
 # Prep the channel folders
 #if [ ! -d "$pdir/channels" ]; then
-if [ ! -d "$channels_path" ]; then
-	mkdir "$channels_path"
+if [ ! -d "${channels_path}" ]; then
+	mkdir "${channels_path}"
 fi
 
 while read line; do
 
 	# If the line is not empty...
-	if [ ! -z "$line" ]; then
+	if [ ! -z "${line}" ]; then
 
 	# Channel name and channel url are stored in channels.txt
-	read -r channel url <<< $(echo $line)
+	read -r channel url <<< $(echo ${line})
 
-	chan_path="$channels_path/$channel"
-	if [ ! -d "$chan_path" ]; then
-		mkdir "$chan_path"
-		touch "$chan_path/dl_archive.txt"
+	chan_path="${channels_path}/${channel}"
+	if [ ! -d "${chan_path}" ]; then
+		mkdir "${chan_path}"
+		touch "${chan_path}/dl_archive.txt"
 	fi
-	cd $chan_path
+	cd ${chan_path}
 
-	video_filters="--dateafter now-"$content_age"days --playlist-end 5"
+	video_filters="--dateafter now-"${content_age}"days --playlist-end 5"
 	output_options="--download-archive dl_archive.txt -o %(upload_date)s%(fulltitle)s.%(ext)s"
 	misc_options="--no-call-home --no-progress -N 2"
 
@@ -53,15 +53,15 @@ while read line; do
 #	video_options="-f bestvideo[height<=1440]+bestaudio"
 	video_options="-x --audio-format mp3"
 
-	options="$video_filters $video_options $output_options $misc_options --ffmpeg-location $ffmpeg_location"
+	options="${video_filters} ${video_options} ${output_options} ${misc_options} --ffmpeg-location ${ffmpeg_location}"
 	# I wanted to keep youtube-dl out of here, but this seems like the best way to make this portable.
-	$pdir/yt-dlp $options -- "$url"
+	${pdir}/yt-dlp "$options" -- "${url}"
 
 	fi
 
-done <$channels_file
+done <${channels_file}
 
-cd "$pdir"
+cd "${pdir}"
 
 
 # Deal with the files.
@@ -69,4 +69,4 @@ cd "$pdir"
 # -r is the age at which to start removing the files.
 # -t is the target directory.
 # These are preset in the script.
-$pdir/sync_files.sh -r $content_age -t "$channels_path"
+${pdir}/sync_files.sh -r ${content_age} -t "${channels_path}"
--- sync_files.sh
+++ sync_files.sh
@@ -29,13 +29,13 @@ file_type="*.mp3"
 
 # Go through the passed options.
 while getopts 'r:t:h' opt; do
-  case "$opt" in
+  case "${opt}" in
     r)
 	# If OPTARG is a number, then remove all older days of that number
-	if [[ $OPTARG == ?(-)+([[:digit:]]) ]]; then
-		file_age="$OPTARG"
-		echo "Removing audio files older than $file_age days."
-	elif [ $OPTARG = "all" ]; then
+	if [[ ${OPTARG} == ?(-)+([[:digit:]]) ]]; then
+		file_age="${OPTARG}"
+		echo "Removing audio files older than ${file_age} days."
+	elif [ ${OPTARG} = "all" ]; then
 	# Else if OPTARG is 'all' then remove all audio files.
 		echo "Removing ALL audio files!"
 		remove_all_files=true
@@ -43,8 +43,8 @@ while getopts 'r:t:h' opt; do
       ;;
 
     t)
-	echo "Target directory is: $OPTARG"
-	preset_target_dir="$OPTARG"
+	echo "Target directory is: ${OPTARG}"
+	preset_target_dir="${OPTARG}"
 
 	# Sanity Check. The path should not be root and it should end with a slash.
 	# if ...
@@ -56,21 +56,21 @@ while getopts 'r:t:h' opt; do
 	echo ""
 	echo "[Removing older files]"
 	echo "Remove audio files older than set days|or remove all."
-	echo "Default preset variable in the code is set to $file_age days."
+	echo "Default preset variable in the code is set to ${file_age} days."
 	echo ""
-	echo "Set with '$0 -r $file_age'"
+	echo "Set with '$0 -r ${file_age}'"
 	echo "To remove all audio files."
 	echo "$0 -r all"
 	echo ""
 	echo "[Change the target directory for storing files]"
-	echo "Default preset variable for the target directory to remove files is $preset_target_dir"
+	echo "Default preset variable for the target directory to remove files is ${preset_target_dir}}"
 	echo "It can be changed with."
-	echo "$0 -t $preset_target_dir"
+	echo "$0 -t ${preset_target_dir}"
       exit 1
       ;;
   esac
 done
-shift "$(($OPTIND -1))"
+shift "$((${OPTIND} -1))"
 
 # Remove all or by age.
 if $remove_all_files; then
@@ -78,16 +78,16 @@ if $remove_all_files; then
 	# if ...
 	echo "Removing files..."
 	#echo $preset_target_dir*/*.mp3
-	rm -rf $preset_target_dir*/*.mp3
+	rm -rf ${preset_target_dir}*/*.mp3
 else
 
 	# Remove all *.mp3 files that are beyond an age, so all old ones are removed and do not fill our storage up.
 	#find "$preset_target_dir" -name "$file_type" -type f -mtime +$file_age
-	find "$preset_target_dir" -name "$file_type" -type f -mtime +$file_age -delete
+	find "${preset_target_dir}" -name "${file_type}" -type f -mtime +${file_age} -delete
 fi
 
 # Sometimes the script borks and leaves partial files behind. Let's remove those.
 # *.part
 # *.ytdl
-find "$preset_target_dir" -name "*.part" -type f -delete
-find "$preset_target_dir" -name "*.ytdl" -type f -delete
+find "${preset_target_dir}" -name "*.part" -type f -delete
+find "${preset_target_dir}" -name "*.ytdl" -type f -delete
